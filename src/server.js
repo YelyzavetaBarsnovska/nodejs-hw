4d1abb18a71610cc1e4bb5a3057efffa04a51871
Yelyzaveta Baranovska <lizabaranovska133@gmail.com>
2026-01-27 14:14:19 +0200

    fix server structure and middlewares

--- src/server.js
+++ src/server.js
@@ -1,64 +1,47 @@
+// src/server.js
+
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+//pino-http для логування HTTP-запитів
+app.use(logger);
+// Middleware для парсингу JSON
 app.use(express.json());
-
+// Дозволяє запити з будь-яких джерел
 app.use(cors());
 
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
-
+// Кореневий маршрут
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello, World!' });
 });
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+app.use(notesRoutes);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
+// Middleware 404 (після всіх маршрутів)
+app.use(notFoundHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+// обробка помилок від celebrate (валідація)
+app.use(errors());
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+// Middleware для обробки помилок (останнє)
+app.use(errorHandler);
 
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: err.message,
-  });
-});
+// підключення до MongoDB
+await connectMongoDB();
 
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
